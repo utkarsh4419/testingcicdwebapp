1e16b34cbad06530fcb94b2437131b21b0510949
utkarsh4419 <utkarsh.sharma4@genpact.com>
2026-01-06 15:37:58 +0530

    prod commit

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
 from flask import Flask, jsonify
  
 app = Flask(__name__)
- 
+
 @app.route("/health", methods=["GET"])
 def health():
-<<<<<<< HEAD
-    return jsonify({"status": "testingrishibhaiprodlatest"})
-=======
-    return jsonify({"status": "testingrishibhaidev is healthy again"})
->>>>>>> UAT
+    return jsonify({"status": "testingprod is healthy again"})
  
 @app.route("/api/hello", methods=["GET"])
 def hello():
