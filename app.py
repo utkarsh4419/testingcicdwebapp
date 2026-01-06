3509f6b3c95fab970447e5f4ededda52c585bf4f
utkarsh4419 <utkarsh.sharma4@genpact.com>
2026-01-06 14:39:52 +0530

    pushing to uat

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+ 
+app = Flask(__name__)
+ 
+@app.route("/health", methods=["GET"])
+def health():
+    return jsonify({"status": "testingrishibhai123"})
+ 
+@app.route("/api/hello", methods=["GET"])
+def hello():
+    return jsonify({"message": "Hello from Azure Web App"})
+ 
+if __name__ == "__main__":
+    app.run()
